87c28c353737187668fdc7775906c5d587c20ded
nfukuma <100755561+NaoyaFukuma@users.noreply.github.com>
2022-12-22 12:37:57 +0900

    done

--- ex03/srcs/bsp.cpp
+++ ex03/srcs/bsp.cpp
@@ -6,7 +6,7 @@
 /*   By: nfukuma <nfukuma@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/21 15:50:20 by nfukuma           #+#    #+#             */
-/*   Updated: 2022/12/21 22:58:42 by nfukuma          ###   ########.fr       */
+/*   Updated: 2022/12/21 23:45:44 by nfukuma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@
 
 bool	bsp(Point const A, Point const B, Point const C, Point const P)
 {
-
 	Point AB(B - A);
 	Point BP(P - B);
 	Point BC(C - B);
@@ -24,13 +23,10 @@ bool	bsp(Point const A, Point const B, Point const C, Point const P)
 	Point CA(A - C);
 	Point AP(P - A);
 
-	Fixed	c1;
-	Fixed	c2;
-	Fixed	c3;
+	Fixed	c1(AB.getX() * BP.getY() - AB.getY() * BP.getX());
+	Fixed	c2(BC.getX() * CP.getY() - BC.getY() * CP.getX());
+	Fixed	c3(CA.getX() * AP.getY() - CA.getY() * AP.getX());
 
-	c1 = AB.getX() * BP.getY() - AB.getY() * BP.getX();
-	c2 = BC.getX() * CP.getY() - BC.getY() * CP.getX();
-	c3 = CA.getX() * AP.getY() - CA.getY() * AP.getX();
 	if ((c1 > 0 && c2 > 0 && c3 > 0) || (c1 < 0 && c2 < 0 && c3 < 0))
 		return true;
 	return false;
